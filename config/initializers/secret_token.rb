a158e8a657d94a7cddd725bffe39183e65e94341
Joel AZEMAR <joel.azemar@gmail.com>
2013-10-16 13:46:42 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aquila::Application.config.secret_key_base = '69abad6bb000edbd6ea8287ff126c29ac14e6103208f31ac5231534401ae8d730f635a84cecea6c364e8e8b5430a74d1910720174018561cbef9611f71912689'
