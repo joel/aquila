18abefe8c95287f01b55ee944d22c1a23aa68683
Joel AZEMAR <joel.azemar@gmail.com>
2013-10-23 11:30:35 +0200

    Change class selector bootstrap 2 for bootstrap 3

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -16,7 +16,7 @@ SimpleForm.setup do |config|
     b.use :placeholder
     b.use :label
     b.wrapper tag: 'div', class: 'controls' do |input|
-      input.wrapper tag: 'div', class: 'input-prepend' do |prepend|
+      input.wrapper tag: 'div', class: 'input-group' do |prepend|
         prepend.use :input
       end
       input.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
@@ -29,7 +29,7 @@ SimpleForm.setup do |config|
     b.use :placeholder
     b.use :label
     b.wrapper tag: 'div', class: 'controls' do |input|
-      input.wrapper tag: 'div', class: 'input-append' do |append|
+      input.wrapper tag: 'div', class: 'input-group' do |append|
         append.use :input
       end
       input.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
