f4634f1aeb52d4f914cc299c75afe43994b072bb
Joel AZEMAR <joel.azemar@gmail.com>
2013-10-25 18:28:52 +0200

    Add strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :authenticate_user!
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:username, :email, :password, :password_confirmation, vault_attributes: [:subdomain])
+    end
+  end
+
   def themes
     @themes ||= %w[cyborg readable]
   end
@@ -13,9 +24,13 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_theme
 
-  around_filter :scope_current_vault
+  around_filter :scope_current_vault, except: [:authenticate_user!] # if user_signed_in?
 
-private
+  private
+
+  def after_sign_up_path_for resource
+    vault_path resource.vault.subdomain
+  end
 
   # def current_user
   #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -23,12 +38,13 @@ private
   # helper_method :current_user
 
   def current_vault
+    return nil unless user_signed_in?
     Vault.where(subdomain: request.subdomain).first || raise('no vault found')
   end
   helper_method :current_vault
 
   def scope_current_vault
-    Vault.current_id = current_vault.id
+    Vault.current_id = current_vault.try :id
     yield
   ensure
     Vault.current_id = nil
