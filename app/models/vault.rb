34012015412e625de814ef4ad98b4e49018920be
Joel AZEMAR <joel.azemar@gmail.com>
2013-11-08 15:02:43 +0100

    Add some constraintes for writing a valid subdomain

--- app/models/vault.rb
+++ app/models/vault.rb
@@ -8,6 +8,9 @@ class Vault
 
   validates :subdomain, presence: true
   validates_uniqueness_of :subdomain, case_sensitive: true, allow_blank: false
+  validates_format_of :subdomain, with: /^[a-z0-9_]+$/, message: 'must be lowercase alphanumerics only', multiline: true
+  validates_length_of :subdomain, maximum: 32, message: 'exceeds maximum of 32 characters'
+  validates_exclusion_of :subdomain, in: ['www', 'mail', 'ftp'], message: 'is not available'
 
   def self.current_id=(id)
     Thread.current[:vault_id] = id
