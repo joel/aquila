a716c0154a6e6bd5cd87a494d8076295f1252272
Joel AZEMAR <joel.azemar@gmail.com>
2013-11-08 15:14:19 +0100

    Set domain on heroku config

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Aquila::Application.config.session_store :cookie_store, key: '_aquila_session', domain: :all
+Aquila::Application.config.session_store :cookie_store, key: '_aquila_session', domain: ENV['DOMAIN']
