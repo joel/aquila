e97dbadc53e1a2aa99f69ec3cf152ed7a381487f
Joel AZEMAR <joel.azemar@gmail.com>
2013-11-08 17:00:27 +0100

    goldbricks need a subdomain

--- spec/routing/goldbricks_routing_spec.rb
+++ spec/routing/goldbricks_routing_spec.rb
@@ -4,31 +4,31 @@ describe GoldbricksController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/goldbricks").should route_to("goldbricks#index")
+      get("http://subdomain.lvh.me/goldbricks").should route_to("goldbricks#index")
     end
 
     it "routes to #new" do
-      get("/goldbricks/new").should route_to("goldbricks#new")
+      get("http://subdomain.lvh.me/goldbricks/new").should route_to("goldbricks#new")
     end
 
     it "routes to #show" do
-      get("/goldbricks/1").should route_to("goldbricks#show", :id => "1")
+      get("http://subdomain.lvh.me/goldbricks/1").should route_to("goldbricks#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/goldbricks/1/edit").should route_to("goldbricks#edit", :id => "1")
+      get("http://subdomain.lvh.me/goldbricks/1/edit").should route_to("goldbricks#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/goldbricks").should route_to("goldbricks#create")
+      post("http://subdomain.lvh.me/goldbricks").should route_to("goldbricks#create")
     end
 
     it "routes to #update" do
-      put("/goldbricks/1").should route_to("goldbricks#update", :id => "1")
+      put("http://subdomain.lvh.me/goldbricks/1").should route_to("goldbricks#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/goldbricks/1").should route_to("goldbricks#destroy", :id => "1")
+      delete("http://subdomain.lvh.me/goldbricks/1").should route_to("goldbricks#destroy", :id => "1")
     end
 
   end
