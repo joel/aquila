972ff35ec633f9b7a2b5da6328f8400b959e5eea
Joel AZEMAR <joel.azemar@gmail.com>
2013-11-13 13:59:59 +0100

    FIX static themes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,32 +2,29 @@ class PagesController < ApplicationController
   skip_before_filter :authenticate_user!
   skip_around_filter :scope_current_vault
 
-  before_filter :theme_names
-
   def cyborg
+    session[:theme] = 'cyborg'
     render :index
   end
 
   def readable
+    session[:theme] = 'readable'
     render :index
   end
 
   def cosmo
+    session[:theme] = 'cosmo'
     render :index
   end
 
   def flatly
+    session[:theme] = 'flatly'
     render :index
   end
 
   def simplex
+    session[:theme] = 'simplex'
     render :index
-  end
-
-  private
-
-  def theme_names
-    @themes ||= %w[cyborg readable cosmo flatly simplex]
   end
 
 end
