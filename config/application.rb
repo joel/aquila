efa9785b35bbad78f4a25b05be4b8d0e8de1e409
Joel AZEMAR <joel.azemar@gmail.com>
2014-01-08 17:09:58 +0100

    Mute deprecated warning

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module Aquila
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :fr
     config.i18n.available_locales = [:en, :fr]
+    config.i18n.enforce_available_locales = false
+    I18n.config.enforce_available_locales = false
 
     # less-rails gem (default all generators)
     config.app_generators.stylesheet_engine :less
--- spec/locales/locales_spec.rb
+++ spec/locales/locales_spec.rb
@@ -4,7 +4,7 @@ describe "#locales" do
 
   Dir.glob('config/locales/**/*.yml') do |locale_file|
     describe "#{locale_file}" do
-      it_behaves_like 'a valid locale file' do
+      it_behaves_like 'my valid locale file' do
         let(:locale_file) { locale_file }
       end
     end
