e81b1f1b751140bf845f92c1c3c0a722eb6dab39
Joel AZEMAR <joel.azemar@gmail.com>
2014-01-08 16:39:55 +0100

    Add feature scenario
    but session is not persisted and signing was lose

--- spec/features/golbrick_resources_spec.rb
+++ spec/features/golbrick_resources_spec.rb
@@ -1,12 +1,12 @@
 require File.dirname(__FILE__) + '/acceptance_helper'
 
-feature 'normal way', js: true, focused: true do
+feature 'normal way' do
   let(:vault)      { create :vault, subdomain: 'me'}
   let(:me)         { create :user, vault: vault }
   let!(:goldbrick) { create :goldbrick, name: 'My Goldbrick', vault: me.vault }
 
-  before { sign_in me }
-  # background { sign_in me }
+  # before { sign_in me }
+  background { sign_in me }
 
   scenario 'should render list of goldbricks' do
     visit goldbricks_url(subdomain: me.vault.subdomain)
@@ -15,22 +15,22 @@ feature 'normal way', js: true, focused: true do
 
   scenario 'should render show goldbrick information' do
     visit goldbrick_url(subdomain: me.vault.subdomain, id: goldbrick)
-    # save_and_open_page
-    # page.should have_text goldbrick.name
     expect(page).to have_content goldbrick.name
     page.should have_link I18n.t('helpers.links.edit')
   end
 
-  scenario 'should ...' do
+  scenario 'should create new one goldbrick' do
     visit edit_goldbrick_url(subdomain: me.vault.subdomain, id: goldbrick)
     expect(page).to have_content 'New Goldbrick'
     current_url.should == edit_goldbrick_url(subdomain: me.vault.subdomain, id: goldbrick)
-    # save_and_open_page
     attributes_for(:goldbrick).each do |key, value|
       fill_in "goldbrick[#{key}]", with: value
     end
     click_button 'Update'
-save_and_open_page
+    # save_and_open_page
+    # expect( page ).to have_content(
+    #   I18n.t('controller.goldbricks.update.success')
+    # )
   end
 
 end
