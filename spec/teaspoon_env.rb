799ac6e3502bffd983909de318f98f9e709d9c98
Joel AZEMAR <joel.azemar@gmail.com>
2014-08-19 16:40:42 +0200

    update teaspoon configuration

--- spec/teaspoon_env.rb
+++ spec/teaspoon_env.rb
@@ -12,7 +12,7 @@ require File.expand_path("../../config/environment", __FILE__)
 #
 # teaspoon --driver=selenium --suppress-log
 # rake teaspoon DRIVER=selenium SUPPRESS_LOG=false
-Teaspoon.setup do |config|
+Teaspoon.configure do |config|
   # Driver / Server
   #config.driver              = "phantomjs" # available: phantomjs, selenium
   #config.server              = nil # defaults to Rack::Server
